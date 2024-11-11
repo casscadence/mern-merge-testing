d50f7bb5d86308aee9349c7b86a14a7a26ae9ac2
casscadence <casscadence@gmail.com>
2024-11-11 05:45:17 -0500

    updated concurrently

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 
 export const getProducts = async (req, res) => {
-
+	
 	try {
 		const products = await Product.find({});
 		res.status(200).json({ success: true, data: products });
